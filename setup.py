77128a4db3952fabb2771ee9d1d99722f6964511
Nicholas Lee <nicholas_lee@berkeley.edu>
2022-10-16 00:56:28 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 setup(name='svg_stack',
       description='stack multiple SVG images into a single output',
-      version='0.1.0', # keep in sync with svg_stack.py
-      author='Andrew Straw <strawman@astraw.com>',
-      url='http://github.com/astraw/svg_stack',
+      version='0.1.1',
+      author='Nicholas Lee <nicholas_lee@berkeley.edu>',
+      url='http://github.com/dragon18456/svg_stack',
       py_modules=['svg_stack'],
       scripts=['svg_stack.py'],
       )
